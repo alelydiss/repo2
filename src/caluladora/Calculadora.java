393ca6d19a4e978be7d39135f67f177259914331
alelydiss <alelydiss@gmail.com>
2024-04-20 15:45:03 +0200

    Inicializar proyecto: Calculadora suma y resta

---
+++ src/caluladora/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caluladora;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Calculadora {
+    
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+    
+    public int restar(int a, int b) {
+        return a - b;
+    }
+    
+}
